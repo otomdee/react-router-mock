54fc3bbc4a239fc1b121e3e681d9f33ff3939820
otomdee <otomdaniel65@gmail.com>
2024-10-31 10:05:10 +0100

    refactor outlet to dynamic segments

---
+++ src/routes.jsx
@@ -0,0 +1,17 @@
+import App from "./App";
+import ErrorPage from "./ErrorPage";
+import Profile from "./Profile";
+
+const routes = [
+    {
+      path: "/",
+      element: <App/>,
+      errorElement: < ErrorPage />
+    },
+    {
+      path: "profile/:name",
+      element: <Profile />,
+    }
+  ]
+
+export default routes;
